e72323e5ab4b88a0232aec0a31aa0b5c410243ed
xiter22222 <pawanpediredla@gmail.com>
2024-12-06 11:14:47 +0530

    Implement invoice builder oogic in invoiceBuilder.js

---
+++ .history/src/invoiceBuilder_20241206111413.js
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) Pawvan.
+ *
+ * Author: Pawvan
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+const stripe = require('stripe')(process.env.STRIPE_API_KEY)
+const createInvoice = async (customerId,items)=>{
+    try{
+const invoice = await stripe.invoices.create({
+    customer:customerId,
+    auto_advance:true,
+    collection_method:'charge_automatically',
+    description:'Invoice for your purchases',
+    amount_due:items.reduce((acc,item)=>acc+item.amount,0),
+    currency:'usd'
+})
+    }
+    catch(error){
+
+        console.error('Error creating invoice :',error)
+    }
+}
+module.exports={createInvoice};
