8275df3323f23e27f946a97cc3c48eca8e524670
xiter22222 <pawanpediredla@gmail.com>
2024-12-06 11:48:53 +0530

    Implement Stripe API integration for customer creation

---
+++ .history/src/stripeApi_20241206114719.js
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) Pawvan.
+ *
+ * Author: Pawvan
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+const stripe = require('stripe')(process.env.STRIPE_API_KEY);
+const createCustomer =async()=>{
+try{}
+catch(error){
+    console.error('Error when creating customers',error);
+}
+}
+module.exports={createCustomer}
